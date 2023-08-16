1d7ed2ec0df4d1a578356d721e1e7d2cd8675724
umair kifayat <umairkifayat3@gmail.com>
2023-08-15 21:42:40 -0700

    function

---
+++ app.js
@@ -0,0 +1,18 @@
+// question no start 2
+
+let first_name = prompt("first name") 
+let last_name = prompt("last name")
+// console.log(first_name,last_name);
+
+function user(first_name,last_name) {
+    console.log(first_name+last_name);
+}
+user(first_name,last_name)
+// question no end 2
+// practice
+
+        function Mcdonalds( drink1 , drink2, drink3 ,adress ) {
+    console.log("Sir ap k ghr deliver ho jy ga jis ma "+drink1+drink2+drink3 + adress+"ha");
+    
+}
+Mcdonalds("Pepsi" ,"vine" ,"bear")
